68da4548dd14ad4678e2af312f13d4b49ab25dc8
yusuf <yusuf@DESKTOP-B6M92UC>
2022-04-02 15:55:43 +0300

    metot demo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi " + musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.Yasi);
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi : " + musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.Yasi);
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri listelendi : " + musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.Yasi);
+        }
+        public void Guncelleme(string musteriAdi, string musteriSoyadi, int musteriYasi)
+        {
+
+            Console.WriteLine("Başarılı güncellendi : " + musteriAdi + " " + musteriSoyadi);
+
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Yusuf";
+            musteri1.Soyadi = "Yıldız";
+            musteri1.TCKimlikNo = "11122233344";
+            musteri1.Yasi = 21;
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Ekrem";
+            musteri2.Soyadi = "Kızılay";
+            musteri2.TCKimlikNo = "44433322211";
+            musteri2.Yasi = 20;
+
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Fatih";
+            musteri3.Soyadi = "Acar";
+            musteri3.TCKimlikNo = "11122233444";
+            musteri3.Yasi = 20;
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.TCKimlikNo);
+
+            }
+            Console.WriteLine("-------------Metotlar-----------");
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            musteriManager.Silme(musteri1);
+            musteriManager.Listele(musteri3);
+
+            musteriManager.Guncelleme("Ekrem","Kızılay",20);
+
+        }
+    }
+}
