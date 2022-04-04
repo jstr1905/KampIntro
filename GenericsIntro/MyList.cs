3e377dbbb46960d71cd1af5a4b7cd05ffe823aa0
yusuf <yusuf@DESKTOP-B6M92UC>
2022-04-04 16:34:52 +0300

    c#

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+    //constructor olduğunu classla aynı isim olduğundan anlarız o da aslında bir metot
+{
+    class MyList<T>//T istediğim elemanın yerini alır 
+                   //T type 
+    {
+        T[] items;
+        //constructor
+        //ctor tab tab yaptık 
+        //bir class newlendiğinde çalışan bloğa constructor deniyor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;//newlediğimiz anda yeni referans numarası aldığı için (o referansı birine tutturmamız lazım) elemanlarımızın kaybolmaması için geçici olarak bir string arraye (tempArray) tutturuyoruz.
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];//
+                                        //değerleri itemse geri alıyoruz.
+            }
+            items[items.Length - 1] = item;//aslında eklemem gereken elemanı şu an ekledim
+        }
+    }
+}
+//"Eğer zorlanmazsan gelişemezsin"
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyList isimler = new MyList(); bu şekilde olmuyor generic class olduğu için içerisinde olan metotlar tip bağımlı çalışıyor
+            //calışıcağım veri tipini ver diyor.
+            MyList<string> isimler = new MyList<string>();//int de verebiliriz class da verebiliriz //class türünde liste
+            isimler.Add("Engin");
+
+        }
+    }
+}
