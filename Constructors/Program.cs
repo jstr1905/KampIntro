45202b9f06602081f35800ae6afb790e599dec8a
yusuf <yusuf@DESKTOP-B6M92UC>
2022-04-08 14:32:57 +0300

    my dictionary

---
+++ Constructors/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer() { Id = 1, FirstName = "Engin", LastName = "Demiroğ", City = "Ankara" };//instance/örnek oluşturduk
+
+            Customer customer2 = new Customer(2, "Derin", "Demiroğ", "Ankara");
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            Console.WriteLine(customer2.FirstName);//ilk başta gelmediği için Id=id City=city yazdık
+
+
+
+        }
+
+
+    }
+
+    class Customer
+    {
+        //default constructor
+        public Customer()
+        {
+
+        }
+
+        public Customer(int id, string firstName, string lastName, string city)//constructor metot gibi çalışır
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string> sehirler = new Dictionary<string>();
+            sehirler.Add("İstanbul");
+            sehirler.Add("Ankara");
+            sehirler.Add("Bursa");
+            sehirler.Add("Balıkesir");
+            Console.WriteLine(sehirler.Count);
+        }
+    }
+}
