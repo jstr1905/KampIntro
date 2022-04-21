512d5b948307cef5e8beab70945d59236bbb75dd
yusuf <yusuf@DESKTOP-6NC5A7V>
2022-04-21 18:49:32 +0300

    c#

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //Method injection dependency injection
+        public void BasvuruYap(IKrediManager krediManager,ILoggerService loggerService)
+        {
+            //Başvuru bilgilerini değerlendirme
+            //
+            krediManager.Hesapla();
+            loggerService.Log();
+
+        }
+
+
+        //listedeki her bir kredinin hesabını yap
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            //KonutKrediManager konutKrediManager = new KonutKrediManager();
+            //konutKrediManager.Hesapla();
+
+
+            //interfaceleri birbirinin alternatifi olan ama kod içerikleri farklı olan durumlar için kullanırız.
+            //Interfaceler de o interfaceyi implemente eden class ın referans numarasını tutabilir.
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager,new FileLoggerService());
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, tasitKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+            //sOlid sisteme yeni bir özellik eklendiğinde mevcut kodlar bozulmaz.
+            //interfaceler referans tutucudur.
+
+
+        }
+    }
+}
